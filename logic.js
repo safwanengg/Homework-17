22a3bcc78c91ed39b925cb56469bb731f2cf9654
safwanengg <safwanengg@gmail.com>
2019-02-24 01:45:35 -0800

    Done

--- logic.js
+++ logic.js
@@ -1,6 +1,5 @@
 // Function to determine marker size based on population
-var queryUrl = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 var cityMarkers = [];
 function markerSize(mag) {
   return mag * 15000;
